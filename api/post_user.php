689332e05823cc17e3e5d282b7bba236a8d5fb02
fran2203 <franciscoutrera@live.com>
2021-02-27 12:17:03 -0300

    Created post_user.php

---
+++ api/post_user.php
@@ -0,0 +1,15 @@
+<?php
+    include('../db_config.php');
+
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+    if($conn->connect_errno) {
+        header('HTTP/1.1 500 Internal Server Error');
+        header('Content-Type: application/json; charset=UTF-8');
+        die(json_encode(array('error' => $conn->connect_error)));
+    }
+
+    $conn->set_charset('utf8');
+
+    var_dump($_POST);
+?>
