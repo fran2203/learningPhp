09c8b872eea08868792b68cdfdec576a9d616d03
fran2203 <franciscoutrera@live.com>
2021-02-27 09:15:50 -0300

    Add fectAll function sendFormulario.js

--- scripts/sendFormulario.js
+++ scripts/sendFormulario.js
@@ -3,41 +3,9 @@ const tbody = document.getElementById('table_body');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-
-    fetch('api/get_users.php')
-        .then(res => res.json())
-        .then(res => {
-            if(res.length > 0){
-                if(tbody.firstChild !== null){
-                    deleteRows();
-                }
-                for (let i = 0; i < res.length; i++) {
-                    const row = document.createElement('tr');
-                    const {nombre, apellido, edad, contraseña} = res[i];
-                    const fragment = document.createDocumentFragment();
-
-                    const td_name = document.createElement('td');
-                    const td_last = document.createElement('td');
-                    const td_age = document.createElement('td');
-                    const td_pass = document.createElement('td');
-
-                    td_name.textContent = nombre;
-                    td_last.textContent = apellido;
-                    td_age.textContent = edad;
-                    td_pass.textContent = contraseña;
-
-                    fragment.append(td_name);
-                    fragment.append(td_last);
-                    fragment.append(td_age);
-                    fragment.append(td_pass);
-
-                    row.append(fragment);
-                    tbody.append(row);
-                }
-            } else {
-                form.before(errorMesage('No hay datos en la base de datos'))
-            }
-        });
+    const valorInput = e.target.valor.value;
+    if(valorInput === '') return fetchAll(); //Si se cumple la condicion, el evento termina acá
+    
 })
 
 function errorMesage(msg) {
@@ -56,4 +24,41 @@ function deleteRows() {
     while (tbody.firstChild !== null) {
         tbody.removeChild(tbody.firstChild);
     }
+}
+
+function fetchAll() {
+    fetch('api/get_users.php')
+    .then(res => res.json())
+    .then(res => {
+        if(res.length > 0){
+            if(tbody.firstChild !== null){
+                deleteRows();
+            }
+            for (let i = 0; i < res.length; i++) {
+                const row = document.createElement('tr');
+                const {nombre, apellido, edad, contraseña} = res[i];
+                const fragment = document.createDocumentFragment();
+
+                const td_name = document.createElement('td');
+                const td_last = document.createElement('td');
+                const td_age = document.createElement('td');
+                const td_pass = document.createElement('td');
+
+                td_name.textContent = nombre;
+                td_last.textContent = apellido;
+                td_age.textContent = edad;
+                td_pass.textContent = contraseña;
+
+                fragment.append(td_name);
+                fragment.append(td_last);
+                fragment.append(td_age);
+                fragment.append(td_pass);
+
+                row.append(fragment);
+                tbody.append(row);
+            }
+        } else {
+            form.before(errorMesage('No hay datos en la base de datos'))
+        }
+    });
 }
