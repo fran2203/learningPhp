a1acecb29684d0a6d20441e4670943ff1be16372
fran2203 <franciscoutrera@live.com>
2021-02-27 09:07:02 -0300

    Add get all parameters get_users.php

--- api/get_users.php
+++ api/get_users.php
@@ -11,43 +11,107 @@
 
     $conn->set_charset('utf8');
 
-    if (isset($_GET['lastName'])) { //Si recibe un parámetro por el metodo GET hace lo siguiente
-        $apellido = $_GET['lastName'];
-        $query = "SELECT * FROM datos_usuario WHERE apellido = '$apellido'";
-        $res = $conn->query($query);
+    if ($_GET['search'] === 'true' ) { //Si parámetro search por metodo GET es true hace lo siguiente
+        if(isset($_GET['lastName'])) { //Entra en el condicional si se pasa lastName por parámetros
+            $apellido = $_GET['lastName'];
+            $query = "SELECT * FROM datos_usuario WHERE apellido = '$apellido'";
+            $res = $conn->query($query);
 
-        $arr = array();
-    
-        for($i=0;$i < $res->num_rows;$i++){
-            array_push($arr, $res->fetch_assoc());
-        }
-
-        $conn->close();
-
-        if(count($arr) === 0) {
-            header('HTTP/1.0 404 Not Found');
-            die();
-        }
-
-        header('Content-Type: application/json; charset=UTF-8');
-        echo json_encode($arr);
-
-    } else {
-        $query = 'SELECT * FROM datos_usuario';
-        $res = $conn->query($query);
+            $arr = array();
         
-        $arr = array();
-    
-        for($i=0;$i < $res->num_rows;$i++){
-            array_push($arr, $res->fetch_assoc());
+            for($i=0;$i < $res->num_rows;$i++){
+                array_push($arr, $res->fetch_assoc());
+            }
+
+            $conn->close();
+
+            if(count($arr) === 0) {
+                header('HTTP/1.0 404 Not Found');
+                die();
+            }
+
+            header('Content-Type: application/json; charset=UTF-8');
+            die(json_encode($arr));
         }
 
-        //Una vez ya obtuve los datos, cierro la conexión
-        $conn->close();
+        if(isset($_GET['name'])){ //Entra en el condicional si se pasa name por parámetros
+            $nombre = $_GET['name'];
+            $query = "SELECT * FROM datos_usuario WHERE nombre = '$nombre'";
+            $res = $conn->query($query);
+
+            $arr = array();
+        
+            for($i=0;$i < $res->num_rows;$i++){
+                array_push($arr, $res->fetch_assoc());
+            }
+
+            $conn->close();
+
+            if(count($arr) === 0) {
+                header('HTTP/1.0 404 Not Found');
+                die();
+            }
+
+            header('Content-Type: application/json; charset=UTF-8');
+            die(json_encode($arr));
+        }
+
+        if(isset($_GET['age'])){
+            $edad = $_GET['age'];
+            $query = "SELECT * FROM datos_usuario WHERE edad = $edad";
+            $res = $conn->query($query);
+
+            $arr = array();
+        
+            for($i=0;$i < $res->num_rows;$i++){
+                array_push($arr, $res->fetch_assoc());
+            }
+
+            $conn->close();
+
+            if(count($arr) === 0) {
+                header('HTTP/1.0 404 Not Found');
+                die();
+            }
+
+            header('Content-Type: application/json; charset=UTF-8');
+            die(json_encode($arr));
+        }
+
+        if(isset($_GET['password'])){
+            $contra = $_GET['password'];
+            $query = "SELECT * FROM datos_usuario WHERE contraseña = '$contra'";
+            $res = $conn->query($query);
+
+            $arr = array();
+        
+            for($i=0;$i < $res->num_rows;$i++){
+                array_push($arr, $res->fetch_assoc());
+            }
+
+            $conn->close();
+
+            if(count($arr) === 0) {
+                header('HTTP/1.0 404 Not Found');
+                die();
+            }
+
+            header('Content-Type: application/json; charset=UTF-8');
+            die(json_encode($arr));
+        }
+    }
+    $query = 'SELECT * FROM datos_usuario';
+    $res = $conn->query($query);
+        
+    $arr = array();
     
-        header('Content-Type: application/json; charset=UTF-8');
-        echo json_encode($arr);
+    for($i=0;$i < $res->num_rows;$i++){
+        array_push($arr, $res->fetch_assoc());
     }
 
-
+    //Una vez ya obtuve los datos, cierro la conexión
+    $conn->close();
+    
+    header('Content-Type: application/json; charset=UTF-8');
+    echo json_encode($arr);
 ?>
